79255ec77e41feda14b96e1fb601d05286ac852a
Landon Beckley <landonbeckley@gmail.com>
2023-02-06 19:47:57 -0500

    movement

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,8 +1,13 @@
+import { BoxObject, ExtendedObject3D } from '@enable3d/common/dist/types'
 import { Scene3D } from '@enable3d/phaser-extension'
 
+
 export default class MainScene extends Scene3D {
+  cursors: any
+  box: ExtendedObject3D
   constructor() {
     super({ key: 'MainScene' })
+
   }
 
   init() {
@@ -14,14 +19,34 @@ export default class MainScene extends Scene3D {
     this.third.warpSpeed()
 
     // adds a box
-    this.third.add.box({ x: 1, y: 2 })
+    this.box = this.third.physics.add.box({ x: 1, y: 2 })
 
-    // adds a box with physics
-    this.third.physics.add.box({ x: -1, y: 2 })
+    this.cursors = this.input.keyboard.createCursorKeys();
+      
 
     // throws some random object on the scene
     this.third.haveSomeFun()
   }
 
-  update() {}
+  update() {
+    
+    if (this.cursors.left.isDown) {
+     this.box.translateX(-20)
+    } else if (this.cursors.right.isDown) {
+      this.box.translateX(20);
+    } 
+    else if (this.cursors.up.isDown) {
+      this.box.translateZ(-20);
+    } 
+    else if (this.cursors.down.isDown) {
+      this.box.translateZ(20);
+    } 
+    else if (this.cursors.space.isDown) {
+      this.box.translateY(20);
+    } 
+    else {
+      this.box.translateY(-10);
+      
+    }
+  }
 }
