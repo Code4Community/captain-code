1c1d486ee51e8ed185e78465f96468273c89da0d
Tassos Manganaris <tassos.manganaris@gmail.com>
2023-02-20 19:06:08 -0500

    Fixed duck loading

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,9 +1,6 @@
 import { Scene3D } from '@enable3d/phaser-extension'
 import duck from "../../assets/Duck.glb";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-// import 
-import duck from './Duck.glb';
-// import Duck from "../../assets/img/Duck.glb"
 // import { GLTFLoader } from '../GLTFLoader.js'
 // import { PhysicsLoader } from '@enable3d/phaser-extension'
 import { PhysicsLoader } from '@enable3d/ammo-physics'
@@ -13,52 +10,53 @@ import { PhysicsLoader } from '@enable3d/ammo-physics'
 
 
 export default class MainScene extends Scene3D {
-  cursors: any
-  box: any
+    cursors: any
+    box: any
 
-  constructor() {
-    super({ key: 'MainScene' })
-  }
-
-  init() {
-    this.accessThirdDimension()
-  }
-
-  create() {
-    // creates a nice scene
-    this.third.warpSpeed()
-
-    // adds a box
-    // this.third.add.box({ x: 1, y: 2 })
-
-    // adds a box with physics
-    // this.third.physics.add.box({ x: -1, y: 2 })
-
-    // throws some random object on the scene
-    // this.third.haveSomeFun()
-
-
-    this.box = this.third.physics.add.box({ x: 5 }, { phong: { color: 'green' } })
-    // this.box.position.setX(200)
-    // this.box.position
-
-
-    new GLTFLoader().loadAsync('Duck.glb').then( gltf => {
-      const duck: any = gltf.scene.children[0]
-      duck.position.z = 6
-      this.scene.add(duck, {})
-    })
-
-    this.third.physics.debug?.enable()
-
-    this.cursors = this.input.keyboard.createCursorKeys()
-
-
-  }
-
-  update() {
-    if (this.cursors.left.isDown) {
-      console.log(this.box.position.z)
+    constructor() {
+        super({ key: 'MainScene' })
     }
-  }
+
+    init() {
+        this.accessThirdDimension()
+    }
+
+    create() {
+        // creates a nice scene
+        this.third.warpSpeed()
+
+        // adds a box
+        // this.third.add.box({ x: 1, y: 2 })
+
+        // adds a box with physics
+        // this.third.physics.add.box({ x: -1, y: 2 })
+
+        // throws some random object on the scene
+        // this.third.haveSomeFun()
+
+
+        this.box = this.third.physics.add.box({ x: 5 }, { phong: { color: 'green' } })
+        // this.box.position.setX(200)
+        // this.box.position
+
+        console.log(this);
+
+        new GLTFLoader().loadAsync(duck).then( gltf => {
+            const duck: any = gltf.scene.children[0]
+            console.log(duck);
+            duck.position.z = 6;
+            // console.log(this.scene);
+            this.third.scene.add(duck);
+        })
+
+        this.third.physics.debug?.enable()
+
+        this.cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update() {
+        if (this.cursors.left.isDown) {
+            console.log(this.box.position.z)
+        }
+    }
 }
