42fea812f778850d0ee8cece24b82bd8b36cf009
Landon Beckley <landonbeckley@gmail.com>
2023-04-10 19:22:46 -0400

    Movement working

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,6 +2,7 @@ import { BoxObject, ExtendedObject3D } from '@enable3d/common/dist/types'
 import { Scene3D } from '@enable3d/phaser-extension'
 import duck from "../../assets/Duck.glb";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Math } from 'phaser';
 // import { GLTFLoader } from '../GLTFLoader.js'
 // import { PhysicsLoader } from '@enable3d/phaser-extension'
 import { PhysicsLoader } from '@enable3d/ammo-physics'
@@ -11,11 +12,21 @@ import { PhysicsLoader } from '@enable3d/ammo-physics'
 
 
 export default class MainScene extends Scene3D {
-    cursors: any
-    box: any
+  cursors: any
+  duck: any
+  box: ExtendedObject3D
+  buttonPressed: boolean
+  directionData:Array<Array<number>>
+  direction:integer
+
 
     constructor() {
         super({ key: 'MainScene' })
+        //2D array for direction data. First two elements of each subarray are x and z decrements, respectively.
+    //The third element is the next index if right rotate, and the fourth is next if left rotate
+    this.directionData = [[1,0,1,3],[0,1,2,0],[-1,0,3,1],[0,-1,0,2]];
+    this.direction = 0;
+
     }
 
     init() {
@@ -42,12 +53,14 @@ export default class MainScene extends Scene3D {
 
         console.log(this);
 
-        new GLTFLoader().loadAsync(duck).then( gltf => {
-            const duck: any = gltf.scene.children[0]
+         new GLTFLoader().loadAsync(duck).then( gltf => {
+            this.duck = gltf.scene.children[0]
             console.log(duck);
-            duck.position.z = 6;
-            // console.log(this.scene);
-            this.third.scene.add(duck);
+            this.duck.position.x = 0;
+            this.duck.position.y = 0;
+            this.duck.position.z = 0;
+            this.duck.rotation.y = Math.PI2/2;
+            this.third.scene.add(this.duck);
         })
 
         this.third.physics.debug?.enable()
@@ -56,8 +69,31 @@ export default class MainScene extends Scene3D {
     }
 
     update() {
-        if (this.cursors.left.isDown) {
-            console.log(this.box.position.z)
+      if (this.cursors.left.isDown) {
+        if(!this.buttonPressed){
+            this.direction = this.directionData[this.direction][3];
+            this.duck.rotation.y += Math.PI2/4;
+            this.buttonPressed = true;
+          }
+  
+  
+      } else if (this.cursors.right.isDown) {
+        if(!this.buttonPressed){
+          this.direction = this.directionData[this.direction][2];
+          this.duck.rotation.y += -Math.PI2/4;
+          this.buttonPressed = true;
         }
+      }
+      else if (this.cursors.up.isDown) {
+        if(!this.buttonPressed){
+          this.duck.position.x = this.duck.position.x - this.directionData[this.direction][0];
+          this.duck.position.z = this.duck.position.z - this.directionData[this.direction][1];
+          this.buttonPressed = true;
+        }
+      }  
+      else{
+        this.buttonPressed = false;
+      }
+  
     }
 }
